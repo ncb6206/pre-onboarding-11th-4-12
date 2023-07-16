e9b06934660bc1b8e2bdf286b6259d7b1a9413a8
ncb6206 <ncb6206@gmail.com>
2023-07-17 04:03:19 +0900

    Feat : useInput 훅 추가

---
+++ src/hooks/useInput.ts
@@ -0,0 +1,22 @@
+import {
+  ChangeEvent,
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useState,
+} from 'react';
+
+type ReturnType<T> = [
+  T,
+  (e: ChangeEvent<HTMLInputElement>) => void,
+  Dispatch<SetStateAction<T>>,
+];
+export default function useInput<T extends string>(
+  initialValue: T,
+): ReturnType<T> {
+  const [value, setValue] = useState(initialValue);
+  const handler = useCallback((event: ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value as T);
+  }, []);
+  return [value, handler, setValue];
+}

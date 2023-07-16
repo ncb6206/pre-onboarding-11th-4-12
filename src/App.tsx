434eeb535315765538c723643a63e71730fa748c
ncb6206 <ncb6206@gmail.com>
2023-07-16 13:41:09 +0900

    Init : 프로젝트 초기세팅

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Rate } from 'antd';
+
+function App() {
+  return (
+    <div>
+      <Rate />
+      해위
+    </div>
+  );
+}
+
+export default App;
---
+++ src/styles/Global.tsx
@@ -0,0 +1,42 @@
+import { Global, css } from '@emotion/react';
+import emotionReset from 'emotion-reset';
+
+const style = css`
+  @import url('https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100;300;400;500;700;900&display=swap');
+
+  ${emotionReset}
+
+  * {
+    margin: 0;
+    padding: 0;
+    font-family: 'Noto Sans KR', sans-serif;
+    box-sizing: border-box;
+    -ms-overflow-style: none;
+    scrollbar-width: none;
+  }
+
+  body {
+    box-sizing: border-box;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #1f2328;
+    background-color: #f6f8fa;
+  }
+
+  button {
+    background: transparent;
+    border: none;
+    padding: 0;
+    cursor: pointer;
+  }
+
+  a {
+    color: #1f2328;
+    outline: none;
+  }
+`;
+
+export default function GlobalStyle() {
+  return <Global styles={style} />;
+}

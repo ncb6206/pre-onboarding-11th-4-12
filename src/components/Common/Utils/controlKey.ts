146a2e4dc673295ce82c0cdc26dbcd5f8f10ed19
ncb6206 <ncb6206@gmail.com>
2023-07-19 15:13:07 +0900

    Style : and 연산 if문에 추가

--- src/components/Common/Utils/controlKey.ts
+++ src/components/Common/Utils/controlKey.ts
@@ -6,21 +6,21 @@ const controlKeys =
   (event: KeyboardEvent<HTMLInputElement>) => {
     const clinicLength = clinicList.length;
 
-    if (event.key === 'ArrowDown') {
-      clinicLength > 0 && clinicLength > maxLength
+    if (event.key === 'ArrowDown' && clinicLength > 0) {
+      clinicLength > maxLength
         ? setFocusId(prev => (prev + 1) % maxLength)
         : setFocusId(prev => (prev + 1) % clinicLength);
     }
-    if (event.key === 'ArrowUp') {
-      clinicLength > 0 && clinicLength > maxLength
+    if (event.key === 'ArrowUp' && clinicLength > 0) {
+      clinicLength > maxLength
         ? setFocusId(prev => (prev - 1 > -1 ? prev - 1 : maxLength - 1))
         : setFocusId(prev => (prev - 1 > -1 ? prev - 1 : clinicLength - 1));
     }
     if (event.key === 'Escape') {
       setFocusId(-1);
     }
-    if (event.key === 'Enter') {
-      clinicLength > 0 && focusId >= 0 && setClinic(clinicList[focusId].sickNm);
+    if (event.key === 'Enter' && clinicLength > 0) {
+      focusId >= 0 && setClinic(clinicList[focusId].sickNm);
     }
   };
 

fb36df53ee3b6c36b23937df255ad14ded06b11f
ncb6206 <ncb6206@gmail.com>
2023-07-19 15:15:41 +0900

    Design : CSS 스타일 변경

--- src/components/List/ClinicList.tsx
+++ src/components/List/ClinicList.tsx
@@ -16,17 +16,31 @@ const ClinicList = ({ clinicList, maxLength, focusId }: IClinicList) => {
             <SearchOutlined />
             <p>{clinic}</p>
           </ClinicListTitle>
-          <span>추천 검색어</span>
-          {clinicList.length !== 0 &&
-            clinicList.slice(0, maxLength).map((clinic, index: number) => (
-              <React.Fragment key={clinic.sickCd}>
-                <ClinicListItem clinic={clinic} focus={focusId === index} />
-              </React.Fragment>
-            ))}
-          {clinicList.length === 0 && <p>검색어 없음</p>}
+          <ClinicListCenter>
+            <span>추천 검색어</span>
+          </ClinicListCenter>
+          <ClinicListUl>
+            {clinicList.length !== 0 &&
+              clinicList.slice(0, maxLength).map((item, index: number) => (
+                <React.Fragment key={item.sickCd}>
+                  <ClinicListLi>
+                    <ClinicListItem item={item} focus={focusId === index} />
+                  </ClinicListLi>
+                </React.Fragment>
+              ))}
+          </ClinicListUl>
+          {clinicList.length === 0 && (
+            <ClinicListNotFound>
+              <span>검색어 없음</span>
+            </ClinicListNotFound>
+          )}
         </>
       )}
-      {!clinic && <span>검색어 없음</span>}
+      {!clinic && (
+        <ClinicListNotFound>
+          <span>검색어 없음</span>
+        </ClinicListNotFound>
+      )}
     </ClinicListDiv>
   );
 };
@@ -36,25 +50,38 @@ const ClinicListDiv = styled.div`
   height: auto;
   display: flex;
   flex-direction: column;
+  padding: 1.5rem 0;
   background-color: #ffffff;
   box-shadow: rgba(30, 32, 37, 0.1) 0px 2px 10px;
   border-radius: 20px;
-  padding: 1.5rem;
   gap: 1rem;
   z-index: 1;
   margin: 0 auto;
-
-  > span {
-    font-size: 1.5rem;
-  }
 `;
 
 const ClinicListTitle = styled.div`
   display: flex;
   flex-direction: row;
+  padding: 0 1.5rem;
   gap: 16px;
   font-size: 20px;
   font-weight: 700;
+`;
+
+const ClinicListCenter = styled.div`
+  padding: 0 1.5rem;
+  font-size: 1.5rem;
+`;
+
+const ClinicListUl = styled.ul`
+  gap: 1rem;
+`;
+
+const ClinicListLi = styled.li``;
+
+const ClinicListNotFound = styled.div`
+  padding: 0 1.5rem;
+  font-size: 1rem;
 `;
 
 export default ClinicList;
